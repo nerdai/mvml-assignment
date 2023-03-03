ec535c1890a758cbab2f9e64c03b00fe47e70114
nerdai <andrei@nerdai.io>
2023-03-03 01:07:27 -0500

    lint get_metrics.py

--- src/get_metrics.py
+++ src/get_metrics.py
@@ -2,3 +2,25 @@
 This file produces the classification metrics report from a supplied labels
 as well as predictions data files.
 """
+
+import pandas as pd
+from sklearn import metrics
+from utils.data_loader import data_loader
+
+
+def produce_report():
+    """
+    This function loads user specified labels and preds files and produces
+    the sklearn metrics.
+    """
+    # load data
+    labels = data_loader("./data/fake_news/labels.csv")
+    preds = data_loader("./preds/predictions.csv")
+
+    # metrics
+    results = pd.merge(preds, labels, on="id")
+    print(metrics.classification_report(results.label, results.pred))
+
+
+if __name__ == "__main__":
+    produce_report()
