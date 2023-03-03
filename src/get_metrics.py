4669193936b688482e9d92a4bdcbc50e96a25b5c
nerdai <andrei@nerdai.io>
2023-03-03 12:32:53 -0500

    add cli args for get_metrics.py

--- src/get_metrics.py
+++ src/get_metrics.py
@@ -3,9 +3,20 @@ This file produces the classification metrics report from a supplied labels
 as well as predictions data files.
 """
 
+import argparse
 import pandas as pd
 from sklearn import metrics
 from utils.data_loader import data_loader
+
+parser = argparse.ArgumentParser(
+    prog='MVML fake news model metrics',
+    description='Takes a batch of predictions and its true labels and produces\
+a classification report.'
+)
+parser.add_argument('--preds', type=str, required=True,
+                    help='path to preds data csv')
+parser.add_argument('--labels', type=str, required=True,
+                    help='path to labels data csv')
 
 
 def produce_report():
@@ -14,8 +25,9 @@ def produce_report():
     the sklearn metrics.
     """
     # load data
-    labels = data_loader("./data/fake_news/labels.csv")
-    preds = data_loader("./preds/predictions.csv")
+    args = parser.parse_args()
+    labels = data_loader(args.labels)
+    preds = data_loader(args.preds)
 
     # metrics
     results = pd.merge(preds, labels, on="id")
