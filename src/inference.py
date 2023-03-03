d0559e9481b09d3aed4f5af05d884b7a0b262f67
nerdai <andrei@nerdai.io>
2023-03-03 12:21:25 -0500

    add cli args to inference.py

--- src/inference.py
+++ src/inference.py
@@ -5,8 +5,17 @@ observations.
 The predictions are saved as .csv in local.
 """
 
+import argparse
 from joblib import load
 from utils.data_loader import data_loader
+
+parser = argparse.ArgumentParser(
+    prog='MVML fake news predictor',
+    description='Takes a batch of news articles text and stores its predictions\
+in a file on local.'
+)
+parser.add_argument('--data', type=str, required=True,
+                    help='path to test data csv')
 
 
 def inference():
@@ -15,10 +24,11 @@ def inference():
     binary for later use.
     """
     # load data
-    new_df = data_loader("./data/fake_news/test.csv")
+    args = parser.parse_args()
+    new_df = data_loader(args.data)
 
     # some cleaning
-    df_clean = new_df[~new_df["text"].isna()]
+    df_clean = new_df[~new_df["text"].isna()].copy()
     df_dirty = new_df[new_df["text"].isna()]
 
     # load & run model
@@ -29,7 +39,7 @@ def inference():
     df_clean["pred"] = preds
     df_clean[["id", "pred"]].to_csv("./preds/predictions.csv", index=False)
 
-    if not df_dirty.empty():
+    if not df_dirty.empty:
         print(f"{df_dirty.shape[0]} observations had no `text` field and thus \
 were not predicted.")
 
